54cdb0724376b3ff97a19d1ca1f69bd52711c299
Yuvika <140506905+yuvika-09@users.noreply.github.com>
2024-04-19 22:40:20 +0530

    Create printPatternUsingLoops.c

---
+++ C/Conditionals and Loops/printPatternUsingLoops.c
@@ -0,0 +1,31 @@
+/* Print a pattern of numbers from 1 to n as shown below. Each of the numbers is separated by a single space.
+4 4 4 4 4 4 4  
+4 3 3 3 3 3 4   
+4 3 2 2 2 3 4   
+4 3 2 1 2 3 4   
+4 3 2 2 2 3 4   
+4 3 3 3 3 3 4   
+4 4 4 4 4 4 4   */
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int len = 2*n - 1;
+    for (int i = 0; i < len; i++) {
+        for (int j = 0; j < len; j++) {
+            int min = i < j ? i : j;
+            min = min < len-i ? min : len-i-1;
+            min = min < len-j-1 ? min : len-j-1;
+            printf("%d ", n-min);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
